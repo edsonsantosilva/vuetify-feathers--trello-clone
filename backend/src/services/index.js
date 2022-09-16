f6fdd5439fc83f29c801d5d14e452729fe683f01
Edson Silva <edsonsantosilva@hotmail.com>
2022-09-16 12:02:23 -0300

    Adding lists service and store

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const boards = require('./boards/boards.service.js');
 const tasks = require('./tasks/tasks.service.js');
+const lists = require('./lists/lists.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(boards);
   app.configure(tasks);
+  app.configure(lists);
 };
